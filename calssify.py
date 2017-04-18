7c08fdda862aed65dbcc9fd934446082cc343395
ShahbalKhan <shahbal_khan@hotmail.com>
2017-04-18 19:45:51 +0500

    updated

--- calssify.py
+++ calssify.py
@@ -6,7 +6,7 @@ from sklearn import preprocessing
 import pandas as pd 
 import numpy as np 
 
-train = pd.read_csv("/home/mma/MMA/FYP/Dataset/id-based-features-and-numeric-train.csv") #load dataframe train
+train = pd.read_csv("/home/mma/MMA/FYP/Dataset/train_numeric.csv") #load dataframe train
 
 train.fillna(0, inplace=True)
 
@@ -28,7 +28,7 @@ X_train = normalizer.transform(X_train)
 Y_train = train.Response.ravel()
 
 # Test data
-test = pd.read_csv("/home/mma/MMA/FYP/Dataset/id-based-features-and-numeric-test.csv") #load test dataframe
+test = pd.read_csv("/home/mma/MMA/FYP/Dataset/test_numeric.csv") #load test dataframe
 test.fillna(0, inplace=True)
 
 #replace nan with numbers in StartTime in test
@@ -61,7 +61,7 @@ X = scaler.transform(X_train)
 #	shuffle=True,verbose=True,learning_rate='adaptive', max_iter=500, validation_fraction=.2)
 
 
-clf = MLPClassifier(activation='relu' ,solver='adam', alpha=0.01, hidden_layer_sizes=(97, 10, 2), random_state=1, 
+clf = MLPClassifier(activation='relu' ,solver='adam', alpha=0.2, hidden_layer_sizes=(97, 10, 2), random_state=1, 
 	shuffle=True,verbose=True,learning_rate='adaptive', max_iter=500, validation_fraction=.2)
 
 print "MLP model training under parameters: \n \n " + str(clf)
