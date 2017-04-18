4b4fa020851191f8afc1e4fa3010d9ebf9b2d4c9
ShahbalKhan <shahbal_khan@hotmail.com>
2017-04-18 22:51:54 +0500

    updated

--- xgbclassifier.py
+++ xgbclassifier.py
@@ -5,28 +5,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 train = pd.read_csv("~/scripts/balanced_train_joined.csv") #load dataframe train
-train.fillna(-9999, inplace=True)
+train.fillna(0, inplace=True)
 
 feats = np.setdiff1d(list(train.columns), ['Id','Response'])
 
-X = np.array(train[feats])
-Y = train.Response.ravel()
-
-seed = 7
-test_size = 0.33
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+X_train = np.array(train[feats])
+y_train = train.Response.ravel()
 
 
+test = pd.read_csv("/home/mma/MMA/FYP/Datasets/id-based-features-and-numeric-test.csv")
+IDS = test.Id
+
+feats = np.setdiff1d(list(test.columns), ['Id'])
+
+X_test = np.array(test[feats])
 
 model = XGBClassifier()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-predictions = [round(value) for value in y_pred]
+pred = pd.DataFrame({'Id': IDS,
+						'Response': y_pred})
 
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
+pred = pred.set_index('Id')
+
+
+pred.to_csv("predictions_mlp.csv")
 
