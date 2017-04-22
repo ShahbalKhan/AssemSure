51e0551c3c71107c5d71580a416671400861d661
ShahbalKhan <shahbal_khan@hotmail.com>
2017-04-22 19:22:59 +0500

    updated

---
+++ sep.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+
+f = open("balanced_train_joined.csv","r")
+g = open("50)balanced_train.csv","w")
+
+a = f.readline()
+g.write(a)
+
+
+count = 0
+line_counter = 0
+
+
+for i in f:
+	x = i.split(",")
+	if x[969] == "0":
+		count += 1
+	if count == 4:
+		g.write(i)
+		line_counter += 1
+		count = 0
+	if x[969] == "1":
+		g.write(i)
+		line_counter += 1
+
+print line_counter
+
+f.close()
+g.close()
