9874cc2444508d6ba97a5e0208ac30f5a5100877
ShahbalKhan <shahbal_khan@hotmail.com>
2017-04-22 20:05:45 +0500

    updated

--- calssify.py
+++ calssify.py
@@ -61,7 +61,7 @@ X = scaler.transform(X_train)
 #	shuffle=True,verbose=True,learning_rate='adaptive', max_iter=500, validation_fraction=.2)
 
 
-clf = MLPClassifier(activation='relu' ,solver='adam', alpha=0.2, hidden_layer_sizes=(80 , 10, 2), random_state=1, 
+clf = MLPClassifier(activation='relu' ,solver='adam', alpha=0.2, hidden_layer_sizes=(100 , 12, 2), random_state=1, 
 	shuffle=True,verbose=True,learning_rate='adaptive', max_iter=500, validation_fraction=.2)
 
 print "MLP model training under parameters: \n \n " + str(clf)
